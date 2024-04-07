d1f75b7f1a13c006445f64674e6d40a1287ec954
Evan Arlian <evanarlian2000@gmail.com>
2024-04-07 23:22:07 +0700

    clean up fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
     fzf_configure_bindings --directory=\cf --processes=\cp --history=\cr --variables=\ce
 end
 
+source $__fish_config_dir/abbr.fish
+source "$HOME/.cargo/env.fish"
+starship init fish | source
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/evan/miniconda3/bin/conda "shell.fish" hook $argv | source
@@ -12,10 +16,7 @@ eval /home/evan/miniconda3/bin/conda "shell.fish" hook $argv | source
 bind \b backward-kill-word # enable ctrl backspace
 bind \e\[3\;5~ kill-word # enable ctrl delete
 
-source $__fish_config_dir/abbr.fish
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/evan/google-cloud-sdk/path.fish.inc' ]
     . '/home/evan/google-cloud-sdk/path.fish.inc'
 end
-
-starship init fish | source
