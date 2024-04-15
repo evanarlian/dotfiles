fbadc0b30de3ffa844b78bd31edd784bfc7f5388
Evan Arlian <evanarlian2000@gmail.com>
2024-04-15 22:36:54 +0700

    use fish nice theme

--- install.sh
+++ install.sh
@@ -67,6 +67,7 @@ stow_all() {
 clean_up() {
     fish -c 'set -U fish_greeting'
     fish -c 'fish_add_path -m ~/.local/bin/'
+    fish -c 'fish_config theme choose "Base16 Eighties"' 
     ~/miniconda3/bin/conda init bash fish
 }
 
