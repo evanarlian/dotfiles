7c071bc70137ecd0b6c6d3bdaf2eaedcb012d56d
Evan Arlian <evanarlian2000@gmail.com>
2024-06-08 18:12:18 +0700

    dynamic username

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,7 @@ source "$HOME/.cargo/env.fish"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/evan/miniconda3/bin/conda "shell.fish" hook $argv | source
+eval /home/$USER/miniconda3/bin/conda "shell.fish" hook $argv | source
 # <<< conda initialize <<<
 
 # Keybinding
@@ -16,8 +16,8 @@ bind \b backward-kill-word # enable ctrl backspace
 bind \e\[3\;5~ kill-word # enable ctrl delete
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/evan/google-cloud-sdk/path.fish.inc' ]
-    . '/home/evan/google-cloud-sdk/path.fish.inc'
+if [ -f "/home/$USER/google-cloud-sdk/path.fish.inc" ]
+    . "/home/$USER/google-cloud-sdk/path.fish.inc"
 end
 
 # starship must be at the very bottom to shadow all prompt modifiers
