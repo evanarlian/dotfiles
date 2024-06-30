80083d41c5134432b6eef1630ef40800e4cd83c9
Evan Arlian <evanarlian2000@gmail.com>
2024-06-30 14:18:16 +0700

    fsh add more complete conda init commands

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,15 @@ source "$HOME/.cargo/env.fish"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/$USER/miniconda3/bin/conda "shell.fish" hook $argv | source
+if test -f /home/$USER/miniconda3/bin/conda
+    eval /home/$USER/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/$USER/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/$USER/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/$USER/miniconda3/bin" $PATH
+    end
+end
 # <<< conda initialize <<<
 
 # Keybinding
