2ac988f8d9582a179b9547c6b527e97a9c426e36
Evan Arlian <evanarlian2000@gmail.com>
2025-06-01 18:56:28 +0700

    add nice fish abbr

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -1,4 +1,19 @@
-abbr --add k kubectl
-abbr --add pm python manage.py
-abbr --add u uv run
-abbr --add um uv run -m
+# python
+abbr --add pm 'python manage.py'
+abbr --add u 'uv run'
+abbr --add um 'uv run -m'
+
+# git 
+abbr --add g 'git'
+abbr --add gst 'git status'
+abbr --add gco 'git checkout'
+abbr --add gb 'git branch'
+abbr --add gcom --set-cursor 'git commit -m "%"'
+abbr --add glog 'git log --graph --oneline --all'
+abbr --add gpus 'git push'
+abbr --add gpul 'git pull'
+
+# misc
+abbr --add c 'code .'
+abbr --add t 'tmux'
+abbr --add k 'kubectl'
