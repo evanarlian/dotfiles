b2f9f29c93b77b8f73f37055380554adcc39bcd5
Evan Arlian <evanarlian2000@gmail.com>
2025-06-01 20:28:02 +0700

    dont overdo abbr

--- fish/.config/fish/abbr.fish
+++ fish/.config/fish/abbr.fish
@@ -5,13 +5,9 @@ abbr --add um 'uv run -m'
 
 # git 
 abbr --add g 'git'
-abbr --add gst 'git status'
 abbr --add gco 'git checkout'
-abbr --add gb 'git branch'
 abbr --add gcom --set-cursor 'git commit -m "%"'
 abbr --add glog 'git log --graph --oneline --all'
-abbr --add gpus 'git push'
-abbr --add gpul 'git pull'
 
 # misc
 abbr --add c 'code .'
