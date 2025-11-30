3f26cf00775059d37bdac8e042f1ad9c1bfb5b36
Evan Arlian <evanarlian2000@gmail.com>
2025-11-30 15:59:08 +0700

    simpler fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,19 @@
 function fish_prompt
+        set -l last_status $status
+
         if test -n "$SSH_TTY"
                 echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
         end
-    
+
         echo -n (set_color blue)(prompt_pwd)' '
-    
+
         set_color -o
         if fish_is_root_user
                 echo -n (set_color red)'# '
+        else if test $last_status -ne 0
+                echo -n (set_color red)'❯ '
+        else
+                echo -n (set_color green)'❯ '
         end
-        echo -n (set_color -d white)'ඞ  '
         set_color normal
 end
